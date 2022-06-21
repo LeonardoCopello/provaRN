bfc5a311a3ea2d4dad3d744fd1395e5f09ca56ea
Leonardo Copello <tencopello@hotmail.com>
2022-06-21 10:13:12 -0300

    mensagem

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
-// import { Container } from './styles';
-
 export default function Footer({ info }) {
   return (
     <View style={styles.container}>
